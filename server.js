c95d88ef034c9e0f079bb5f8c91fe18d66faf750
Suyash Borkar <suyashborkar2017@gmail.com>
2025-05-02 01:04:09 +0530

    Weather App Backend

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 10000;
+
+// Middleware to set security headers
+app.use(helmet());
+
+// Middleware to log HTTP requests
+app.use(morgan('dev'));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
