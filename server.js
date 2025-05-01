58b62001064448f8afc67e59ec81d22ea99195cf
Suyash Borkar <suyashborkar2017@gmail.com>
2025-05-02 02:00:58 +0530

    Weather App Backend

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import cors from 'cors';
-
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,11 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 // Middleware
+app.use(cors()); // Enable CORS
 app.use(helmet());
 app.use(morgan('dev'));
-
-// Enable CORS for all routes
-app.use(cors());
 
 // Serve static files from 'public' (including index.html)
 app.use(express.static(path.join(__dirname, 'public')));
